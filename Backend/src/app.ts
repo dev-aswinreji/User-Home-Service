eba059e27fb7fcffe83d12a5fdcb59fcc566e5cc
Aswin Reji <aswinreji.004@gmail.com>
2025-01-07 20:28:04 +0000

    feat: user home service made as a repo and added to github as a new repo

---
+++ Backend/src/app.ts
@@ -0,0 +1,17 @@
+import express, { NextFunction } from "express";
+import { Request, Response } from "express";
+import route from "./route/userRoute";
+import cors from "cors"
+const PORT = process.env.PORT || 4001;
+console.log(PORT, "Port is showing here");
+const app = express();
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cors<Request>())
+
+app.use("/", route);
+
+app.listen(PORT, () => {
+  console.log(`Server is running http://localhost:${PORT}/home`);
+});
---
+++ Frontend/src/pages/home.tsx
@@ -0,0 +1,18 @@
+import { useEffect, useState } from "react"
+
+export default function Home() {
+    const [home, setHome] = useState("no data found")
+    useEffect(() => {
+        fetch("http://localhost:4001/home")
+            .then((res) => res.json())
+            .then(data => setHome(data.message))
+    }, [home])
+
+    return (
+        <>
+            <div className="text-center font-bold py-11">
+                {home}
+            </div>
+        </>
+    )
+}
